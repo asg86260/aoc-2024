86f6e36c3b5d9de126c9a9d313754be24060ce49
Andrew <asg86260@gmail.com>
2024-12-07 14:57:42 -0600

    day 7 done

--- 7/index.ts
+++ 7/index.ts
@@ -1,301 +1,103 @@
 import input, { example } from './input'
 
-type Matrix = string[][]
-type TrackerMatrix = { visited: boolean; directions: Direction[] }[][]
-type Position = {
-  row: number
-  column: number
+function add(val1: number, val2: number): number {
+  return val1 + val2
 }
 
-type Direction = 'N' | 'E' | 'S' | 'W'
-
-function isTrackerMatrix(
-  matrix: Matrix | TrackerMatrix,
-): matrix is TrackerMatrix {
-  return !!matrix[0][0]
+function multiply(val1: number, val2: number): number {
+  return val1 * val2
 }
 
-function buildMatrix(string: string): Matrix {
-  const charMatrix: Matrix = []
-  const rows = string.split('\n')
-
-  for (const row of rows) {
-    charMatrix.push(row.split(''))
-  }
-
-  return charMatrix
+function concat(val1: number, val2: number): number {
+  return Number(val1.toString().concat(val2.toString()))
 }
 
-const guard = '^'
+const operators = {
+  '+': add,
+  '*': multiply,
+  '||': concat,
+}
 
-function findGuardPosition(matrix: Matrix): Position | undefined {
-  for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
-    for (
-      let columnIndex = 0;
-      columnIndex < matrix[rowIndex].length;
-      columnIndex++
-    ) {
-      if (matrix[rowIndex][columnIndex] === guard) {
-        return { row: rowIndex, column: columnIndex }
-      }
+type Equation = {
+  expect: number
+  numbers: number[]
+}
+
+const results: number[] = []
+function factorial(n: number) {
+  if (n === 0 || n === 1) return 1
+  if (results[n] > 0) return results[n]
+  results[n] = factorial(n - 1) * n
+  return results[n]
+}
+
+function buildEquationArray(input: string): Equation[] {
+  return input.split('\n').map((equation) => {
+    const parts = equation.split(':')
+    return {
+      expect: Number(parts[0]),
+      numbers: parts[1].trim().split(' ').map(Number),
     }
-  }
-}
-
-let direction: Direction = 'N'
-type Status = 'DONE' | 'CLEAR' | 'OBSTACLE' | 'LOOP'
-
-const position = { row: 0, column: 0 }
-
-function step(tracker: Matrix | TrackerMatrix) {
-  const newPosition = { row: position.row, column: position.column }
-  switch (direction) {
-    case 'N':
-      newPosition.row--
-      break
-    case 'E':
-      newPosition.column++
-      break
-    case 'S':
-      newPosition.row++
-      break
-    case 'W':
-      newPosition.column--
-      break
-    default:
-      console.log('what direction are you going?')
-      break
-  }
-
-  updateCurrentPosition(newPosition)
-  if (isTrackerMatrix(tracker)) {
-    logPositionAndDirection(newPosition, tracker)
-  } else {
-    logPosition(newPosition, tracker)
-  }
-}
-
-function logPosition(newPosition: Position, tracker: Matrix) {
-  tracker[newPosition.row][newPosition.column] = 'X'
-}
-
-function logPositionAndDirection(
-  newPosition: Position,
-  tracker: TrackerMatrix,
-) {
-  const directions = tracker[newPosition.row][newPosition.column].directions
-  tracker[newPosition.row][newPosition.column] = Object.assign({
-    visited: true,
-    directions: [...directions, direction],
   })
 }
 
-function check(
-  matrix: Matrix,
-  position: Position,
-  tracker?: TrackerMatrix,
-): Status {
-  let checkPosition = Object.assign({ ...position })
-  switch (direction) {
-    case 'N':
-      checkPosition = {
-        column: position.column,
-        row: position.row - 1,
-      }
-      break
-    case 'E':
-      checkPosition = {
-        column: position.column + 1,
-        row: position.row,
-      }
-      break
-    case 'S':
-      checkPosition = {
-        column: position.column,
-        row: position.row + 1,
-      }
-      break
-    case 'W':
-      checkPosition = {
-        column: position.column - 1,
-        row: position.row,
-      }
-      break
-    default:
-      console.log('what direction are you going?')
+function buildOperators(
+  size: number,
+  permutations = [],
+  array = [],
+): string[][] {
+  for (const operator in operators) {
+    if (size === 0) {
+      return array.push(permutations)
+    }
+    buildOperators(size - 1, [operator, ...permutations], array)
   }
 
-  if (
-    !matrix[checkPosition.row] ||
-    !matrix[checkPosition.row][checkPosition.column]
-  ) {
-    return 'DONE'
-  }
+  return array
+}
 
-  if (tracker) {
-    if (
-      tracker[checkPosition.row][checkPosition.column].directions.includes(
-        direction,
-      )
-    ) {
-      return 'LOOP'
+function testEquation(equation: Equation): number {
+  const operatorCount = equation.numbers.length - 1
+  const operatorCombinations = buildOperators(operatorCount)
+
+  for (const combo of operatorCombinations) {
+    // console.log('testing: ', combo, equation.numbers)
+
+    let total = 0
+    for (let i = 0; i < equation.numbers.length; i++) {
+      // console.log('index', i, total, combo[i - 1], equation.numbers[i])
+      if (i === 0) {
+        total = equation.numbers[i]
+        continue
+      }
+      if (combo[i - 1] === '+') {
+        total = operators['+'](total, equation.numbers[i])
+      }
+
+      if (combo[i - 1] === '*') {
+        total = operators['*'](total, equation.numbers[i])
+      }
+
+      if (combo[i - 1] === '||') {
+        total = operators['||'](total, equation.numbers[i])
+      }
+    }
+
+    if (total === equation.expect) {
+      return equation.expect
     }
   }
+}
 
-  if (
-    matrix[checkPosition.row][checkPosition.column] === '.' ||
-    matrix[checkPosition.row][checkPosition.column] === '^'
+function day7() {
+  const equations = buildEquationArray(input)
+
+  console.log(
+    equations
+      .map(testEquation)
+      .filter(Boolean)
+      .reduce((total, current) => total + current, 0),
   )
-    return 'CLEAR'
-  if (matrix[checkPosition.row][checkPosition.column] === '#') return 'OBSTACLE'
 }
 
-function changeDirection() {
-  switch (direction) {
-    case 'N':
-      direction = 'E'
-      break
-    case 'E':
-      direction = 'S'
-      break
-    case 'S':
-      direction = 'W'
-      break
-    case 'W':
-      direction = 'N'
-      break
-  }
-}
-
-function updateCurrentPosition(newPosition: Position) {
-  position.row = newPosition.row
-  position.column = newPosition.column
-}
-
-function buildTracker(matrix: Matrix): Matrix {
-  const rows = matrix.length
-  const columns = matrix[0].length
-  const trackingMatrix = []
-
-  for (let row = 0; row < rows; row++) {
-    trackingMatrix[row] = Array(columns)
-  }
-
-  return trackingMatrix
-}
-
-function countSteps(tracker: Matrix): number {
-  let total = 0
-  for (const row in tracker) {
-    for (const column in tracker[row]) {
-      if (tracker[row][column] === 'X') total++
-    }
-  }
-  return total
-}
-
-function day6() {
-  const matrix = buildMatrix(example)
-  const tracker = buildTracker(matrix)
-
-  const startingPosition = findGuardPosition(matrix)
-
-  if (!startingPosition) {
-    console.log('No Guard?')
-    return
-  }
-
-  updateCurrentPosition(startingPosition)
-  logPosition(startingPosition, tracker)
-  let status: Status = check(matrix, position)
-  while (status !== 'DONE') {
-    status = check(matrix, position)
-    if (status === 'CLEAR') {
-      step(tracker)
-    } else if (status === 'OBSTACLE') {
-      changeDirection()
-    }
-  }
-  const totalSteps = countSteps(tracker)
-  console.log('Total Steps: ', totalSteps)
-}
-
-// day6()
-
-function buildDirectionTracker(matrix: Matrix): TrackerMatrix {
-  const rows = matrix.length
-  const columns = matrix[0].length
-  const trackingMatrix = []
-
-  for (let row = 0; row < rows; row++) {
-    trackingMatrix[row] = Array(columns).fill({
-      visited: false,
-      directions: [],
-    })
-  }
-  return trackingMatrix
-}
-
-function countTrackerSteps(tracker: TrackerMatrix): number {
-  let total = 0
-  for (const row in tracker) {
-    for (const column in tracker[row]) {
-      if (tracker[row][column].visited) {
-        total++
-      }
-    }
-  }
-  return total
-}
-
-function day6Part2() {
-  const matrix = buildMatrix(input)
-  const startingPosition = findGuardPosition(matrix)
-
-  if (!startingPosition) {
-    console.log('No Guard?')
-    return
-  }
-
-  let loopCount = 0
-  for (const row in matrix) {
-    for (const column in matrix[row]) {
-      const testMatrix = matrix.map((arr) => arr.slice())
-      if (
-        startingPosition.column === Number(column) &&
-        startingPosition.row === Number(row)
-      ) {
-        continue
-      }
-
-      if (testMatrix[row][column] === '#') {
-        continue
-      }
-
-      testMatrix[row][column] = '#'
-
-      updateCurrentPosition(startingPosition)
-      direction = 'N'
-      const testTracker = buildDirectionTracker(testMatrix)
-      logPositionAndDirection(startingPosition, testTracker)
-      let status: Status = check(testMatrix, position, testTracker)
-
-      while (status !== 'DONE') {
-        status = check(testMatrix, position, testTracker)
-        if (status === 'CLEAR') {
-          step(testTracker)
-        } else if (status === 'OBSTACLE') {
-          changeDirection()
-        } else if (status === 'LOOP') {
-          loopCount++
-          break
-        }
-      }
-      console.log('total steps', countTrackerSteps(testTracker))
-    }
-  }
-
-  console.log('Total Obstacles ', loopCount)
-}
-
-day6Part2()
+day7()
